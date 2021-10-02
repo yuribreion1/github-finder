2856349347a41977e1ce4f7c963b6d5e8f0e52f5
Yuri Breion <yuribreion@br.ibm.com>
2021-10-02 20:18:35 -0300

    calling the navbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
+import Navbar from './components/layout/Navbar';
 import React, { Component } from 'react'
 
 class App extends Component{
   render() {
     return (
-      <div className="App">
-        <h1>Hello world</h1>
-      </div>
+      <nav className="App">
+        <Navbar title='Github Finder' icon='fab fa-github'/>
+      </nav>
     ); 
   }
 }
