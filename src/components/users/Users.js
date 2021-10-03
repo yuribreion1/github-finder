72f4908a4a161292161e547bd146fc9dec2d645f
Yuri Breion <yuribreion@br.ibm.com>
2021-10-03 19:45:49 -0300

    refactoring to function

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,51 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import UserItem from './UserItem'
+import { Spinner } from '../layout/Spinner';
 
-class Users extends Component {
-    state = {
-        users: [
-            {
-                "login": "mojombo",
-                "id": 1,
-                "node_id": "MDQ6VXNlcjE=",
-                "avatar_url": "https://avatars.githubusercontent.com/u/1?v=4"
-              },
-              {
-                "login": "defunkt",
-                "id": 2,
-                "node_id": "MDQ6VXNlcjI=",
-                "avatar_url": "https://avatars.githubusercontent.com/u/2?v=4"
-              },
-              {
-                "login": "pjhyett",
-                "id": 3,
-                "node_id": "MDQ6VXNlcjM=",
-                "avatar_url": "https://avatars.githubusercontent.com/u/3?v=4"
-              },
-              {
-                "login": "wycats",
-                "id": 4,
-                "node_id": "MDQ6VXNlcjQ=",
-                "avatar_url": "https://avatars.githubusercontent.com/u/4?v=4"
-              },
-              {
-                "login": "ezmobius",
-                "id": 5,
-                "node_id": "MDQ6VXNlcjU=",
-                "avatar_url": "https://avatars.githubusercontent.com/u/5?v=4"
-              }
-        ]
-    }
-    render() {
-        return (
-            <div style={ userStyle }>
-                { this.state.users.map(user => (
-                    <UserItem key={ user.key } user={ user } />
-                    )
-                ) }
-            </div>
-        );
-    }
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner/>
+  } else {
+    return (
+      <div style={ userStyle }>
+          { users.map(user => (
+              <UserItem key={ user.key } user={ user } />
+              )
+          ) }
+      </div>
+    )
+  }
 }
 
 const userStyle = {
