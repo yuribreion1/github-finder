f5a2685f5bcc159bbdd00c7f934682ab83691b1c
Yuri Breion <yuribreion@br.ibm.com>
2021-10-05 08:16:05 -0300

    adding search component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import React, { Component } from 'react'
 import axios from 'axios';
 
 class App extends Component{
-  state = {
-    users: [],
-    loading: false
-  }
-  async componentDidMount() {
-    this.setState({ loading: true })
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    this.setState({ users: res.data, loading: false })
-  }
-  
   render() {
     return (
       <nav className="App">
         <Navbar title='Github Finder' icon='fab fa-github'/>
+        <Search/>
         <Users loading={ this.state.loading } users={ this.state.users } />
       </nav>
     ); 
+  }
+  
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true })
+    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({ users: res.data, loading: false })
   }
 }
 
