8ae99b01217b6f7a285a2ba5ffc187262efe0592
Yuri Breion <yuribreion@br.ibm.com>
2021-10-05 08:16:45 -0300

    adding return to the function

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -2,7 +2,11 @@ import React, { Fragment } from 'react'
 import spinner from './spinner.gif'
 
 export const Spinner = () => {
-    <Fragment>
-        <img src={ spinner } alt="" style={{ widt: '200px', margin: 'auto', display: 'block' }} />
-    </Fragment>
+    return (
+        <Fragment>
+            <img src={ spinner } alt="" style={{ width: '200px', margin: 'auto', display: 'block' }} />
+        </Fragment>
+    );
 }
+
+export default Spinner;
