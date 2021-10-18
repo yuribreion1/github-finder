a3da05c580a4d451d58a6903f327837fa1ed327b
Yuri Breion <yuribreion@br.ibm.com>
2021-10-17 21:33:24 -0300

    Remove the function to call all users to use searc

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component{
     return (
       <nav className="App">
         <Navbar title='Github Finder' icon='fab fa-github'/>
-        <Search/>
+        <Search searchUsers={this.searchUsers} />
         <Users loading={ this.state.loading } users={ this.state.users } />
       </nav>
     ); 
@@ -21,10 +21,10 @@ class App extends Component{
     loading: false
   };
 
-  async componentDidMount() {
+  searchUsers = async text => {
     this.setState({ loading: true })
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    this.setState({ users: res.data, loading: false })
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({ users: res.data.items, loading: false })
   }
 }
 
