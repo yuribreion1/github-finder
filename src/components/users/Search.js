ce9e37aa724f9e5d1211d065b8101b1d2f7400df
Yuri Breion <yuribreion@br.ibm.com>
2021-10-24 00:38:10 -0300

    Calling alert

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -6,7 +6,8 @@ export class Search extends Component {
     static propTypes = {
         searchUsers: PropTypes.func.isRequired,
         clearUsers: PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired
+        showClear: PropTypes.bool.isRequired,
+        setAlert: PropTypes.func.isRequired,
     }
 
     state = {
@@ -17,11 +18,16 @@ export class Search extends Component {
 
     onSubmit = ( e ) => {
         e.preventDefault();
-        this.props.searchUsers(this.state.text);
-        this.setState({ text: ''})
+        if (this.state.text === '') {
+          this.props.setAlert('Please enter something', 'light')
+        } else {
+          this.props.searchUsers(this.state.text);
+          this.setState({ text: ''})
+        }
     }
 
     render() {
+      const { showClear, clearUsers } = this.props;
         return (
             <div>
                 <form onSubmit={ this.onSubmit } className="form">
@@ -34,7 +40,7 @@ export class Search extends Component {
                     />
                     <input type="submit" value="Submit" className="btn btn-dark btn-block" />
                 </form>
-                {this.props.showClear && <button className="btn btn-light btn-block" onClick={this.props.clearUsers}>Clear</button>}
+                {showClear && <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>}
             </div>
         )
     }
