ee42647dc7f336efe02f9bf8c5d39156879211aa
Yuri Breion <yuribreion@br.ibm.com>
2021-10-23 21:38:38 -0300

    Calling clear function

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import Search from './components/users/Search';
 import React, { Component } from 'react'
 import axios from 'axios';
 
-class App extends Component{
+class App extends Component {
+
   render() {
     return (
       <nav className="App">
         <Navbar title='Github Finder' icon='fab fa-github'/>
-        <Search searchUsers={this.searchUsers} />
+        <Search
+          searchUsers={this.searchUsers}
+          clearUsers={this.clearUsers}
+          showClear={this.state.users.length > 0 ? true : false}
+          />
         <Users loading={ this.state.loading } users={ this.state.users } />
       </nav>
-    ); 
+    );
   }
-  
+
   state = {
     users: [],
     loading: false
@@ -23,9 +28,12 @@ class App extends Component{
 
   searchUsers = async text => {
     this.setState({ loading: true })
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    this.setState({ users: res.data.items, loading: false })
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+      this.setState({ users: res.data.items, loading: false })
   }
+
+  clearUsers = () => this.setState({ users: [], loading: false })
+
 }
 
 export default App;
